0fb3ce07a7ecd5b932f9775632f5f8620f19ca9a
SUEN <s-asuen@lwsd.org>
2023-11-30 13:02:43 -0800

    #15 just starting the set up for key press processing and its relation to physics

---
+++ test_server/key_press.py
@@ -0,0 +1,9 @@
+class Player:
+    self.
+
+#bool is whether the key is up or down, direction is either a 0, 1, 2, or 3, representing up, down, left, and right respectively 
+#should create a player class that stores 6 key numbers, the vertical and horizontal positioning on the map
+#for both x and y there has to be a velocity and acceleration 
+#i might also need to store an angular veloctiy or some number that relates to how much they're turning 
+def num_processing(direction: int,  down: bool):
+    
