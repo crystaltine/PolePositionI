e84af3203401ae1f928bb8a122b5de4b76ebf2ff
ConfettiRaindrop <camilafigueira@outlook.com>
2023-12-07 13:19:41 -0800

    add grass and mountains to screen, will check is rsize

--- py_pygame/test_file.py
+++ py_pygame/test_file.py
@@ -13,13 +13,17 @@ screen = pygame.display.set_mode([width,height])
 screen.fill(SKY)
 pygame.display.set_caption("Game")
 size = pygame.display.get_desktop_sizes()
-
+grass = pygame.image.load('grasse.png')
+grass = pygame.transform.scale(grass, (width, 2 * height/3))
+mtns = pygame.image.load('mtns.png')
 FPS = pygame.time.Clock()
 #player = User(Sprite) insert sprite later
 
 #call object instances outside the loop
 while running:
     FPS.tick(24) #moved timer into loop
+    screen.blit(grass, (0, 400))
+    screen.blit(mtns, (0, 200))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
