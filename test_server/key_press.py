10283f3e633d24661ba184c5e5f15e7de3229a62
SUEN <s-asuen@lwsd.org>
2023-12-07 13:28:05 -0800

    #15 added some functionality to updating the position, velocity, and acceleration, left and right processing will happen later

--- test_server/key_press.py
+++ test_server/key_press.py
@@ -6,20 +6,37 @@ class Player:
     self.acceleration
     self.angle
 
-
+    def __init__(self):
+        self.x = 0 
+        self.y = 0
+        self.velocity = 0 
+        self.acceleration = 0 
+        self.angle = 0 
 #something to bring up to the team, how will we cover going off the track, will we allow them to go on the grass? what does that mean for my physics calculations?
-    def num_processing(direction: int,  down: bool):
+    def num_processing(self, direction: int,  down: bool):
+        player1 = Player()
         """
         bool is whether the key is up or down, direction is either a 0, 1, 2, or 3, representing up, down, left, and right respectively 
         should create a player class that stores 6 key numbers, the vertical and horizontal positioning on the map
         for both x and y there has to be a velocity and acceleration 
         i might also need to store an angular veloctiy or some number that relates to how much they're turning 
         """
-        #use while loop that runs 24 times a second to have the vehicle constantly accelerating or decelerating 
-        pass
+        #implementation for the left and right arrow keys to be added later 
+        if down:
+            if direction == 0:
+                player1.acceleration(True)
+            elif direction == 1:
+                player1.acceleration(False)
+            player1.update()
+        else:
+            #if the key is up and the key pressed was either forwards or backwards, default the acceleration to be something slightly negative to simulate air resistance 
+            if direction == 0 or direction == 1:
+                self.accerlation = -2
+        
 
 
-    def angular_acceleration():
+
+    def angular_acceleration(self,):
         """
         method based on speed that determines how fast the user is able to turn
         implementation is similar to driving in most games. Don't want to implement losing traction, just limiting how much you can turn based on the velocity of user. 
@@ -28,14 +45,22 @@ class Player:
         pass
 
     
-    def acceleration(accelerate: bool):
+    def acceleration(self, accelerate: bool):
         """
         method that defines how much you accelerate based on your speed, higher speed = lower acceleration and vice versa
         accerlate is a bool that indicates if the person is accerlerating or decelerating
         the equation is different, our acceleration is based on the speed but decelerating will be constant the boolean is used to differentiate between the 2 
         make sure that it's not possible for the velocity to exceed 100
         """
+        #setting acceleration based on the boolean and the velocity
         if not accelerate:
             self.acceleration = -10
         else:
             self.accerlation = math.sqrt(100 - self.velocity)
+    
+    def update(self):
+        self.velocity += self.acceleration
+        self.x += self.velocity
+    
+
+        
