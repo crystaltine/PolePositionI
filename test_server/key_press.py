0e17947b42c8894b7d1365c95ea69c9b2475d976
SUEN <s-asuen@lwsd.org>
2024-01-09 13:20:17 -0800

    working more on the angles but I have to implement change in y position

--- test_server/key_press.py
+++ test_server/key_press.py
@@ -13,6 +13,7 @@ class Player:
         self.angle_acceleration = 0
         self.angle = 0 
 #something to bring up to the team, how will we cover going off the track, will we allow them to go on the grass? what does that mean for my physics calculations?
+    #passes in direction of key as well as if it is down or not 
     def num_processing(self, direction: int,  down: bool):
         player1 = Player()
         """
@@ -29,14 +30,19 @@ class Player:
                 player1.acceleration(True)
             elif direction == 1:
                 player1.acceleration(False)
+                #right now the method header has a boolean on if the angle change is to be right or not this might be changed but that is why False is passed in since 2 is left
             elif direction == 2:
-                player1.set_angle()
+                player1.set_angle(False)
+            else:
+                player1.set_angle(True)
 
             player1.update()
         else:
             #if the key is up and the key pressed was either forwards or backwards, default the acceleration to be something slightly negative to simulate air resistance 
             if direction == 0 or direction == 1:
                 self.x_accerlation = -2
+            elif direction == 2 or direction == 2:
+                self.angle_acceleration = 0
         
 
 
@@ -47,7 +53,8 @@ class Player:
         implementation is similar to driving in most games. Don't want to implement losing traction, just limiting how much you can turn based on the velocity of user. 
         lower velocity results in higher angle change per seond vice versa for high
         """
-        pass
+        denominator = 0.1 * self.x_velocity + 2.22 
+        self.angle_acceleration = 10/denominator + .5
 
     
     def acceleration(self, accelerate: bool):
@@ -63,7 +70,7 @@ class Player:
         else:
             self.x_accerlation = math.sqrt(100 - self.velocity)
         
-    
+    #have to think about how you change the y as well based on the angle 
     def update(self):
         self.x_velocity += self.x_acceleration
         self.x += self.x_velocity
