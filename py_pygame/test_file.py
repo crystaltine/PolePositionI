2ff2be3824c8172cb21ba4932ee02ec8ee8ed486
Sindhura-Satti <s-ssatti@lwsd.org>
2024-01-11 13:08:22 -0800

    working on main menu

--- py_pygame/test_file.py
+++ py_pygame/test_file.py
@@ -8,6 +8,7 @@ pygame.init()
 running = True
    
 SKY = (97, 120, 232)
+BLACK = (0,0,0)
 
 width = pygame.display.get_desktop_sizes()[0][0]
 height = pygame.display.get_desktop_sizes()[0][1]
@@ -16,46 +17,51 @@ screen = pygame.display.set_mode([width,height])
 screen.fill(SKY)
 pygame.display.set_caption("Game")
 # size = pygame.display.get_desktop_sizes()
+font = pygame.font.Font('freesansbold.ttf', 32)
 grass = pygame.image.load('grasse.png')
 grass = pygame.transform.scale(grass, (int(width), int(2 * height/3)))
 mtns = pygame.image.load('mtns.png')
 mtns = pygame.transform.scale(mtns, (width*2, height/5))
-FPS = pygame.time.Clock()
+#FPS = pygame.time.Clock()
+#FPS.tick(24) #moved timer into loop 
 # player = User(Sprite) insert sprite later
-# def start_screen(displayStart: bool):
-#     screen.fill((0,0,0))
-#     while running and displayStart:
-#         x,y = pygame.mouse.get_pos() 
-#         startButton = pygame.Rect(100,200, 50,50)
-#         if startButton.collidepoint(x,y):
-#             displayStart = False 
 
+menu_text = font.render('Start playing asp_3', True, BLACK)
+menu_rect = menu_text.get_rect(center=(640,260))    #does rect take center as parameter     
+
+def main_menu(): # start screen 
+    while running: 
+        mouse_pos = pygame.mouse.get_pos()
+        screen.fill("white")
+        
+
+        
             
 
-
+def play(): # game screen
 #call object instances outside the loop
-while running:
-    FPS.tick(24) #moved timer into loop
-    # start_screen(True)
-    screen.blit(grass, (0, 2*height/5))
-    screen.blit(mtns, (0, height/5))
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        # escape button works 
-        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            running = False
+    while running:
+        
+        # start_screen(True)
+        screen.blit(grass, (0, 2*height/5))
+        screen.blit(mtns, (0, height/5))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            # escape button works 
+            if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                running = False
             
-    pygame.display.update()
+        pygame.display.update()
 
-    #key numbers for keydowbs binary onkeyevent -> send to server
-    #up: 00
-    #down: 01
-    #left: 10
-    #right: 11
+        #key numbers for keydowbs binary onkeyevent -> send to server
+        #up: 00
+        #down: 01
+        #left: 10
+        #right: 11
 
-    #keydown: 1##
-    #keyup: 0##
+        #keydown: 1##
+        #keyup: 0##
 
    
 pygame.quit()
