4cf49151d890221a59bf9e9573575405c419df64
ConfettiRaindrop <camilafigueira@outlook.com>
2024-01-21 20:51:31 -0800

    move gifs to assets folder, add spritesheet capapbilities is buggy atm
    currentlt project cannot find files in assets folder, looking for
    solution, and while testing something is up with spritesheet is breaking
    - find out what future me/others

--- game/test_file.py
+++ game/test_file.py
@@ -2,6 +2,8 @@ import pygame
 import sys
 import random
 from key_listener import capture_keypress_loop
+import spritesheet
+from sprite_strip_anim import SpriteStripAnim
 
 pygame.init()
 
@@ -16,14 +18,14 @@ height = pygame.display.get_desktop_sizes()[0][1]
 screen = pygame.display.set_mode([width,height])
 screen.fill(SKY)
 pygame.display.set_caption("Game")
-# size = pygame.display.get_desktop_sizes()
 font = pygame.font.Font('freesansbold.ttf', 32)
-grass = pygame.image.load('grasse.png')
-grass = pygame.transform.scale(grass, (int(width), int(2 * height/3)))
-mtns = pygame.image.load('mtns.png')
-mtns = pygame.transform.scale(mtns, (width*2, height/5))
-#FPS = pygame.time.Clock()
-#FPS.tick(24) #moved timer into loop 
+#grass = pygame.image.load('../../assets/grasse.png')
+#grass = pygame.transform.scale(grass, (int(width), int(2 * height/3)))
+#mtns = pygame.image.load('..\\..\\assets\\mtns.png')
+#mtns = pygame.transform.scale(mtns, (width*2, height/5))
+#road = pygame.image.load('straightRoad.gif')
+road = SpriteStripAnim('straightRoad.gif', (0, -height/5, width, 4*height/3), 12, 4, False, 1000)
+#road = pygame.transform.scale(road, (width, 4*height/3))
 # player = User(Sprite) insert sprite later
 
 menu_text = font.render('Start playing asp_3', True, BLACK)
@@ -33,18 +35,14 @@ def main_menu(): # start screen
     while running: 
         mouse_pos = pygame.mouse.get_pos()
         screen.fill("white")
-        
-
-        
-            
 
 def play(): # game screen
 #call object instances outside the loop
     while running:
-        
         # start_screen(True)
-        screen.blit(grass, (0, 2*height/5))
-        screen.blit(mtns, (0, height/5))
+        #screen.blit(grass, (0, 2*height/5))
+        #screen.blit(mtns, (0, height/5))
+        #screen.blit(road, (0, -height/5))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
@@ -53,16 +51,6 @@ def play(): # game screen
                 running = False
             
         pygame.display.update()
-
-        #key numbers for keydowbs binary onkeyevent -> send to server
-        #up: 00
-        #down: 01
-        #left: 10
-        #right: 11
-
-        #keydown: 1##
-        #keyup: 0##
-
    
 pygame.quit()
 sys.exit()
