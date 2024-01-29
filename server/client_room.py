3e20dcf6f08572417715463d5698dcdd14d5d95f
Angus Tian <56783429+atian7064@users.noreply.github.com>
2024-01-28 17:42:25 -0800

    Changed default pos to 0,0

--- server/client_room.py
+++ server/client_room.py
@@ -145,7 +145,7 @@ class Room:
         # testing: put them at (100, 100), (100, 150), (100, 200), and so on.
         # also they should have hitbox radius 2.5
         for i, client in enumerate(self.clients.values()):
-            e = self.world.create_entity(client["username"], client["color"], client["client_obj"], (100, 100 + (50 * i)), hitbox_radius=2.5)
+            e = self.world.create_entity(client["username"], client["color"], client["client_obj"], (0 + (-50 * i), 0), hitbox_radius=2.5)
             client["client_obj"].entity = e
 
     def start_game(self):
