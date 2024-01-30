3668369145357ed3d3b904140fd134bccd646f27
SuenAidan <s-asuen@lwsd.org>
2024-01-29 17:58:42 -0800

    cleaning up code and adding better documentation

--- game/world/entity.py
+++ game/world/entity.py
@@ -67,18 +67,13 @@ class Entity:
         #convert nanosecond difference to seconds
         delta_time_s = (time_ns() - self.last_update_timestamp) / 1e9
         
-        
-        
-        # use the angle to determine components of acceleration
-        # self.acc_mag = 2*self.key_presses[0] - 2*self.key_presses[1]
-        # self.acc[0] = self.acc_mag * math.cos(math.radians(self.angle%360))
-        # self.acc[1] = self.acc_mag * math.sin(math.radians(self.angle%360))
+
 
         #total velocity
         total_vel = math.sqrt(self.vel[0] ** 2 + self.vel[1] ** 2)
 
         # update acceleration, required for calculations below
-        #if statement used redundantly to see if only braking is pressed
+        #if statement used to see if only braking is pressed
         if self.key_presses[1] and not self.key_presses[0]:
             self.acc = -10
         #conditional to simulate drag if neither backwards or forwards is pressed
@@ -87,29 +82,31 @@ class Entity:
         else:
             #when multiplying by a boolean True acts as a 1 and False acts as 0
             #these references to self.key_presses are to deal with forward and backwards being pressed 
-            #at the same time, or none. It ensures correct behavior without many lines added 
+            #at the same time, or none. It ensures correct behavior based on the keys that are being pressed
             self.acc = math.sqrt(100*self.key_presses[0] - 100*self.key_presses[1] - total_vel)
 
 
 
-        #Aidan change: making max turning 20 degrees a second and not allowing the user to turn if they are stopped
         #if statement to deal with corner case of not allowing user to turn while not moving, this is to stop users from going backwards
         if total_vel == 0:
             self.angle += 0
         else:
-            #50 * True or False guarantees that turning has the correct behavior based on which keys are pressed down in any 
-            #if none are pressed then it has an angle change of 0, if both then 0, only difference is when one is pressed and not the other
+            #calculations to simulate how much you can turn based on your speed
+            #faster = slower turning and vice versa for slower speed
             denominator = 0.1 * total_vel+ 2.22 
-            #value that changes how much a person can turn based on speed 
             angular_accel = 10/denominator + .5
+
             #same logic above with the acceleration, self.key_presses[2] increases angle and self.key_presses[3] decreases
             #done to handle multiple keys pressed at once 
+            #50 * True or False guarantees that turning has the correct behavior based on which keys are pressed down in any 
             self.angle += (angular_accel*self.key_presses[2] - angular_accel*self.key_presses[3]) * delta_time_s
-        #set angle back down 
+        
+        #since angle is constantly being added to, % will reset the angle back down to in range of 0-360 degrees so trig calculations are correct
         self.angle %= 360
 
         #add to total velocity with the acceleration and how long it was pressed for, maybe a change needed since the acceleration doesn't change 
         #until the next tick im pretty sure the conversion is based on just multiplying by the time for all but talk to michael
+        #max function used to ensure that players are unable to reverse 
         total_vel = max(total_vel + self.acc * delta_time_s, 0)
 
         self.vel[0] = total_vel * math.cos(math.radians(self.angle))
@@ -120,7 +117,6 @@ class Entity:
         self.pos[0] += self.vel[0] * delta_time_s
         self.pos[1] += self.vel[1] * delta_time_s
 
-        #TODO add the drag mechanic from key_press
         
         self.last_update_timestamp = time_ns()
     
